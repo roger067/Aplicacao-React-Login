fd61eecb6b01bf0bc83714fc95f1fac010b7d661
roger067 <rosestolato.rogerio@gmail.com>
2019-02-01 19:44:57 -0200

    ajustando css

--- src/modules/users/UserForm.js
+++ src/modules/users/UserForm.js
@@ -5,8 +5,8 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import Card from "../../components/Card";
 import BoxTitle from "../../components/BoxTitle";
-import { save, getById } from './userAction';
-import { get as getProducts} from '../products/productAction'
+import { save, getById } from "./userAction";
+import { get as getProducts } from "../products/productAction";
 class UserForm extends React.Component {
   constructor(props) {
     super(props);
@@ -14,7 +14,7 @@ class UserForm extends React.Component {
     this.state = {
       selectedProduct: null,
       products: []
-    }
+    };
   }
 
   componentDidMount() {
@@ -35,11 +35,15 @@ class UserForm extends React.Component {
     }
   }
 
-  renderInput({input, placeholder, type}) {
+  renderInput({ input, placeholder, type }) {
     return (
       <div className="form-group">
         <input
-          type={type} className="form-control" {...input} placeholder={placeholder} required
+          type={type}
+          className="form-control"
+          {...input}
+          placeholder={placeholder}
+          required
         />
       </div>
     );
@@ -56,7 +60,7 @@ class UserForm extends React.Component {
       products.push(selectedProduct._id);
 
       this.setState({ products });
-    }    
+    }
   }
 
   onSubmit = async formProps => {
@@ -67,9 +71,9 @@ class UserForm extends React.Component {
     const response = await this.props.save(formProps);
 
     if (response) {
-      this.props.history.push('/User');
+      this.props.history.push("/User");
     }
-  }
+  };
 
   parseProducts(products) {
     const userProducts = [];
@@ -101,26 +105,61 @@ class UserForm extends React.Component {
             <form onSubmit={handleSubmit(this.onSubmit)}>
               <div className="row">
                 <div className="col-sm-12 col-xs-12">
-                  <Field name="email" component={this.renderInput} type="email" placeholder="Digite o e-mail..."/>
+                  <Field
+                    name="email"
+                    component={this.renderInput}
+                    type="email"
+                    placeholder="Digite o e-mail..."
+                  />
                 </div>
                 <div className="col-sm-12 col-xs-12">
-                  <Field name="name" component={this.renderInput} type="text" placeholder="Digite o nome..."/>
+                  <Field
+                    name="name"
+                    component={this.renderInput}
+                    type="text"
+                    placeholder="Digite o nome..."
+                  />
                 </div>
                 <div className="col-sm-12 col-xs-12">
-                  <Field name="password" component={this.renderInput} type="password" placeholder="Digite a senha..."/>
+                  <Field
+                    name="password"
+                    component={this.renderInput}
+                    type="password"
+                    placeholder="Digite a senha..."
+                  />
                 </div>
                 <div className="col-sm-12 col-xs-12">
-                  <Field name="phoneNumber" component={this.renderInput} type="text" placeholder="Digite o Telefone..."/>
+                  <Field
+                    name="phoneNumber"
+                    component={this.renderInput}
+                    type="text"
+                    placeholder="Digite o Telefone..."
+                  />
                 </div>
 
                 <div className="col-sm-12 col-xs-12">
-                  <div style={{ marginBottom: '2rem' }}>
-                    <select className="form-control" onChange={e => this.selectProduct(e.target.value)}>
-                      { products.map(product => (
-                        <option key={product._id} value={JSON.stringify(product)}>{product.name}</option>
+                  <div style={{ marginBottom: "2rem" }}>
+                    <select
+                      className="form-control"
+                      onChange={e => this.selectProduct(e.target.value)}
+                    >
+                      {products.map(product => (
+                        <option
+                          key={product._id}
+                          value={JSON.stringify(product)}
+                        >
+                          {product.name}
+                        </option>
                       ))}
                     </select>
-                    <button type="button" className="btn" style={{ marginTop: '1rem' }} onClick={() => this.addProduct()}>Adicionar</button>
+                    <button
+                      type="button"
+                      className="btn"
+                      style={{ marginTop: "1rem" }}
+                      onClick={() => this.addProduct()}
+                    >
+                      Adicionar
+                    </button>
                   </div>
 
                   <table className="table table-striped">
@@ -133,20 +172,26 @@ class UserForm extends React.Component {
                       </tr>
                     </thead>
                     <tbody>
-                      { userProducts.map(product => (
+                      {userProducts.map(product => (
                         <tr key={product._id}>
                           <td>{product.name}</td>
                           <td>{product.price}</td>
                           <td>{product.createDate}</td>
-                          <td><button type="button" className="btn btn-danger"><i className="glyphicon glyphicon-trash"></i></button></td>
+                          <td>
+                            <button type="button" className="btn btn-danger">
+                              <i className="glyphicon glyphicon-trash" />
+                            </button>
+                          </td>
                         </tr>
                       ))}
                     </tbody>
                   </table>
-                  
-                  <div className="col-sm-3 col-sm-offset-9" style={{ marginTop: '3rem' }}>
-                    <button className="btn btn-info">Confirmar</button>
-                  </div>
+                </div>
+                <div
+                  className="col-sm-3 col-sm-offset-9"
+                  style={{ marginTop: "3rem" }}
+                >
+                  <button className="btn btn-info">Confirmar</button>
                 </div>
               </div>
             </form>
@@ -166,4 +211,7 @@ const mapStateToProps = state => ({
   products: state.products.list
 });
 
-export default connect(mapStateToProps, { save, getById, getProducts })(UserForm);
+export default connect(
+  mapStateToProps,
+  { save, getById, getProducts }
+)(UserForm);
